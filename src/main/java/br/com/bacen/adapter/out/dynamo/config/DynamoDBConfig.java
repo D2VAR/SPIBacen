48a6233ec9a38a6dc52cd877fd8464124fdf2a5f
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 09:33:01 -0800

    feat: adiciona config dynamoDB

---
+++ src/main/java/br/com/bacen/adapter/out/dynamo/config/DynamoDBConfig.java
@@ -0,0 +1,54 @@
+package br.com.bacen.adapter.out.dynamo.config;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Value("${aws.access.key.id}")
+    private String awsAccessKeyId;
+
+    @Value("${aws.access.key.secret}")
+    private String awsAcessKeySecret;
+
+    @Value("${dynamodb.service.endpoint}")
+    private String dynamoDBServiceEndPoint;
+
+    @Value("${dynamodb.service.region}")
+    private String dynamoDBRegion;
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+        return new DynamoDBMapper(amazonDynamoDB);
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(endPointConfiguration())
+                .withCredentials(credentialsProvider())
+                .build();
+    }
+
+    private AWSCredentialsProvider credentialsProvider() {
+        return new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(awsAccessKeyId, awsAcessKeySecret)
+        );
+    }
+
+    private AwsClientBuilder.EndpointConfiguration endPointConfiguration() {
+        return new AwsClientBuilder.EndpointConfiguration(dynamoDBServiceEndPoint, dynamoDBRegion);
+    }
+}
