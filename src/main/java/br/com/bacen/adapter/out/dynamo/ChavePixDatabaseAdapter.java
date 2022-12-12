ad8a91412fb6619129693f0faae438e9a1d82379
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 09:16:26 -0800

    feat: adiciona database adapter out e entity chave pix

---
+++ src/main/java/br/com/bacen/adapter/out/dynamo/ChavePixDatabaseAdapter.java
@@ -0,0 +1,24 @@
+package br.com.bacen.adapter.out.dynamo;
+
+import br.com.bacen.adapter.out.dynamo.entity.ChavePixEntity;
+import br.com.bacen.domain.mapper.ChavePixMapper;
+import br.com.bacen.domain.model.ChavePix;
+import br.com.bacen.port.out.DatabaseOutputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ChavePixDatabaseAdapter implements DatabaseOutputPort {
+
+    private final ChavePixRepository repository;
+    private final ChavePixMapper mapper;
+
+    @Override
+    public Optional<ChavePix> findChavePixByValor(String valor_chave) {
+        Optional<ChavePixEntity> chavePix = repository.findChavePixByValorChave(valor_chave);
+        return chavePix.map(mapper::modelToDomain);
+    }
+}
