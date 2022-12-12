b8cde6d92757e4a710cb889aa9d8993d0b967ca4
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 09:29:49 -0800

    chore: adiciona enable dynamoDB na classe main

--- src/main/java/br/com/bacen/Application.java
+++ src/main/java/br/com/bacen/Application.java
@@ -1,11 +1,13 @@
 package br.com.bacen;
 
 import lombok.extern.slf4j.Slf4j;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class Application {
 
     public static void main(String[] args) {
