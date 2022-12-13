2639464b9deb1f4b6933d313c27003fe843ee1e5
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 16:12:27 -0800

    feat: adiciona DTO da chave pix consumida do topico

---
+++ src/main/java/br/com/spi/domain/dto/ChavePixDTO.java
@@ -0,0 +1,21 @@
+package br.com.spi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChavePixDTO {
+
+    private String codBanco;
+    private String numeroConta;
+    private String agenciaConta;
+    private String cpfCnpj;
+    private String nome;
+    private String tipoChave;
+    private String valorChave;
+}
