2f40f3ea8b8efd7ab1c77e94211b3d1c3c18986c
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 16:24:55 -0800

    feat: altera diretorio mapper e validator

--- src/main/java/br/com/spi/domain/service/VerificacaoChavePixService.java
+++ src/main/java/br/com/spi/domain/service/VerificacaoChavePixService.java
@@ -2,7 +2,7 @@ package br.com.spi.domain.service;
 
 import br.com.spi.adapter.exception.ChavePixNaoEncontradaException;
 import br.com.spi.domain.dto.ChavePixResponse;
-import br.com.spi.domain.mapper.ChavePixMapper;
+import br.com.spi.infrastructure.mapper.ChavePixMapper;
 import br.com.spi.domain.model.ChavePix;
 import br.com.spi.port.in.VerificacaoChavePixInputPort;
 import br.com.spi.port.out.DatabaseOutputPort;
