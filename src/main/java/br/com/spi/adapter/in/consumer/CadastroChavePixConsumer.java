142b2202692f1d191dba69840ff814b916fa5e49
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 16:17:52 -0800

    feat: adiciona consumer topico cadastro chave pix

---
+++ src/main/java/br/com/spi/adapter/in/consumer/CadastroChavePixConsumer.java
@@ -0,0 +1,37 @@
+package br.com.spi.adapter.in.consumer;
+
+
+import br.com.spi.domain.dto.ChavePixDTO;
+import br.com.spi.infrastructure.validator.ChaveValidator;
+import br.com.spi.port.in.CadastroChavePixInputPort;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CadastroChavePixConsumer {
+
+    private final ChaveValidator chaveValidator;
+    private final CadastroChavePixInputPort inputPort;
+
+    @KafkaListener(topics = "{spring.kafka.topic.name}")
+    public void listen(ConsumerRecord<String, String> mensagemKafka, Acknowledgment ack) {
+        try {
+            log.info(String.format("#### Mensagem Consumida -> %s, topic -> %s",
+                    mensagemKafka.value(), mensagemKafka.topic()));
+            ChavePixDTO chavePixDTO = new ObjectMapper().readValue(mensagemKafka.value(), ChavePixDTO.class);
+            chaveValidator.validate(chavePixDTO);
+            inputPort.cadastrarChave(chavePixDTO);
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+        } finally {
+            ack.acknowledge();
+        }
+    }
+}
