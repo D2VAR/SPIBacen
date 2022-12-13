22184e845ce8df087890ca00c882e4bb79adce3b
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-12 16:23:57 -0800

    feat: adiciona validacao chave pix e exception

---
+++ src/main/java/br/com/spi/adapter/exception/ChaveValidatorException.java
@@ -0,0 +1,17 @@
+package br.com.spi.adapter.exception;
+
+public class ChaveValidatorException extends RuntimeException{
+
+    public ChaveValidatorException() {
+        super();
+    }
+
+    public ChaveValidatorException(String message) {
+        super(message);
+    }
+
+    public ChaveValidatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
