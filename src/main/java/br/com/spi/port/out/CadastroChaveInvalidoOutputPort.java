b2c9a1e8612120a0c7522f40840cd1a75403a959
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-13 08:49:32 -0800

    fix: adicionando producer cadastro chave pix

--- src/main/java/br/com/spi/port/out/CadastroChaveInvalidoOutputPort.java
+++
@@ -1,9 +0,0 @@
-package br.com.spi.port.out;
-
-import br.com.spi.domain.model.ChavePix;
-
-import java.util.Optional;
-
-public interface CadastroChaveInvalidoOutputPort {
-    void notificaErroCadastro(Optional<ChavePix> chavePix);
-}
