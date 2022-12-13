8c33df6663fd9de810bd50b3825063727fb76433
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-13 08:48:14 -0800

    feat: criando mensagem a ser produzida no cadastro chave pix

---
+++ src/main/java/br/com/spi/domain/dto/ChavePixCadastroMensagem.java
@@ -0,0 +1,23 @@
+package br.com.spi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChavePixCadastroMensagem {
+
+
+    private String status;
+    private String codBanco;
+    private String numeroConta;
+    private String agenciaConta;
+    private String cpfCnpj;
+    private String nome;
+    private String tipoChave;
+    private String valorChave;
+}
