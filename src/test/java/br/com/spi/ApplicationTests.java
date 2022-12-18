b0338f3bd4241b30bcc4fe11a442cb594dc96b9e
romosken <rodrigo.mosken@outlook.com>
2022-12-17 23:44:21 -0300

    retirada docker-compose e padronizacao dns kafka

--- src/test/java/br/com/spi/ApplicationTests.java
+++ src/test/java/br/com/spi/ApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.spi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package br.com.spi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
