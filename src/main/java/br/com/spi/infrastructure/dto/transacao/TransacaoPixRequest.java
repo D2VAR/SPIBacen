52bd800f401a527532590b27971283b723ac1f03
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-18 22:10:18 -0800

    chore: adiciona consumer transação pix e dto request

---
+++ src/main/java/br/com/spi/infrastructure/dto/transacao/TransacaoPixRequest.java
@@ -0,0 +1,26 @@
+package br.com.spi.infrastructure.dto.transacao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransacaoPixRequest {
+    @JsonProperty("transaction_id")
+    private String transactionId;
+    @JsonProperty("nome")
+    private String nome;
+    @JsonProperty("cpf_cnpj")
+    private String cpfCnpj;
+    @JsonProperty("chave_destino")
+    private String chaveDestino;
+    @JsonProperty("valor_transferencia")
+    private BigDecimal valor;
+    @JsonProperty("codigo_banco_destino")
+    private String codBancoDestino;
+}
