ad1b02ad5ced174e39b17f33670513d6f769b3eb
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-19 14:37:34 -0800

    fix: altera método de retorno validação

--- src/main/java/br/com/spi/port/out/ValidacaoTransacaoInputPort.java
+++ src/main/java/br/com/spi/port/out/ValidacaoTransacaoInputPort.java
@@ -3,7 +3,5 @@ package br.com.spi.port.out;
 import br.com.spi.infrastructure.dto.transacao.TransacaoValidadaRequest;
 
 public interface ValidacaoTransacaoInputPort {
-
-    void retornarSucesso(TransacaoValidadaRequest request);
-    void retornarFalha(TransacaoValidadaRequest request);
+    void retornarValidacao(TransacaoValidadaRequest request);
 }
