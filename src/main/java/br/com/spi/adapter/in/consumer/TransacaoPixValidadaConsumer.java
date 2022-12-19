cd4fc34508929bacc10d408a54781497679b3262
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-19 08:06:57 -0800

    fix: altera nome do consumer de retorno da validação

--- src/main/java/br/com/spi/adapter/in/consumer/TransacaoPixValidadaConsumer.java
+++ src/main/java/br/com/spi/adapter/in/consumer/TransacaoPixValidadaConsumer.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ValidacaoTransacaoPixConsumer {
+public class TransacaoPixValidadaConsumer {
 
     private final ValidacaoTransacaoInputPort inputPort;
 
