f75ca0fb32e84545df315627e27cc42bacb5b540
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-19 15:24:21 -0800

    chore: adicionando exceptions de parseamento

---
+++ src/main/java/br/com/spi/exception/TransacaoPixParseException.java
@@ -0,0 +1,9 @@
+package br.com.spi.exception;
+
+public class TransacaoPixParseException extends RuntimeException {
+
+    public TransacaoPixParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
