a51000580ced74c950877e87ba9ff34e647897ac
Victoria Bastos <victoria.rezbastos@gmail.com>
2022-12-18 22:35:59 -0800

    chore: adicionando consumer validação transação pix

---
+++ src/main/java/br/com/spi/adapter/in/consumer/ValidacaoTransacaoPixConsumer.java
@@ -0,0 +1,50 @@
+package br.com.spi.adapter.in.consumer;
+
+import br.com.spi.infrastructure.dto.transacao.TransacaoValidadaRequest;
+import br.com.spi.port.out.ValidacaoTransacaoInputPort;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidacaoTransacaoPixConsumer {
+
+    private final ValidacaoTransacaoInputPort inputPort;
+
+    @KafkaListener(id="group-validacao-bacen", topics = "${topic.name.recebedor.retorno.success}")
+    public void listenSuccess(ConsumerRecord<String, String> mensagemKafka, Acknowledgment ack) {
+        try {
+            var request = processConsumerRecord(mensagemKafka);
+            inputPort.retornarSucesso(request);
+        } catch (JsonProcessingException ex) {
+            log.error("#### Error consuming message -> {},{}", ex.getMessage(), ex.getStackTrace());
+        } finally {
+            ack.acknowledge();
+        }
+    }
+
+    @KafkaListener(id="group-validacao-bacen", topics = "${topic.name.recebedor.retorno.fail}")
+    public void listenFail(ConsumerRecord<String, String> mensagemKafka, Acknowledgment ack) {
+        try {
+            var request = processConsumerRecord(mensagemKafka);
+            inputPort.retornarFalha(request);
+        } catch (JsonProcessingException ex) {
+            log.error("#### Error consuming message -> {},{}", ex.getMessage(), ex.getStackTrace());
+        } finally {
+            ack.acknowledge();
+        }
+    }
+
+    private TransacaoValidadaRequest processConsumerRecord(ConsumerRecord<String, String> mensagemKafka) throws JsonProcessingException{
+        log.info("#### Message consumed -> {}, topic -> {}", mensagemKafka.value(), mensagemKafka.topic());
+        var request = new ObjectMapper().readValue(mensagemKafka.value(), TransacaoValidadaRequest.class);
+        return request;
+    }
+}
